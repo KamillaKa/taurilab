76de1ed60c963704a35709028267697282de43e0
Kamilla <KamillaKa@users.noreply.github.com>
2025-01-15 10:07:17 +0200

    Implement video stream setup in Camera component

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -14,9 +14,24 @@ const Camera = forwardRef<HTMLVideoElement, CameraProps>((props, ref) => {
 
     useEffect(() => {
         const setupVideoInput = async () => {
-            videoRef.current?.srcObject = stream;
+          try {
+            if (videoRef.current) {
+              const stream = await navigator.mediaDevices.getUserMedia({
+                video: {
+                  width,
+                  height,
+                },
+                audio: false,
+              });
+              videoRef.current.srcObject = stream;
+              videoRef.current.onloadedmetadata = () => {
+                videoRef.current?.play();
+              };
+            }
+          } catch (error) {}
         };
-    }, []); 
+        setupVideoInput();
+      }, []);
 
   return (
     <video ref={videoRef} width={width} height={height}/>
